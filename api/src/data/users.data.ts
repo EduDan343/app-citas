ceca22b7aaf7e6e1110dc45b99cc0babd6ce184d
Eduardo <dan_999@hotmail.es>
2023-10-24 23:57:58 -0600

    create default values for users and horarios in db

---
+++ api/src/data/users.data.ts
@@ -0,0 +1,227 @@
+export const usersData = [
+    {
+        name: "user 1",
+        lastname: "lastname 1",
+        phone: 678910,
+        citas: [
+            {
+                horario: "08:00 - 09:00",
+                lunes: "Reunion semanal user 1"
+            },
+            {
+                horario: "09:00 - 10:00",
+                martes: "Inicio de actividades user 1"
+            },
+            {
+                horario: "10:00 - 11:00",
+                miercoles: "Reunion con equipo user 1"
+            },
+            {
+                horario: "11:00 - 12:00",
+                jueves: "Desayuno user 1"
+            },
+            {
+                "horario": "12:00 - 13:00",
+            },
+            {
+                horario: "13:00 - 14:00",
+                viernes: "Actualizar actividades user 1"
+            },
+            {
+                horario: "14:00 - 15:00",
+            },
+            {
+                horario: "15:00 - 16:00",
+                miercoles: "Hora de comida user 1"
+            },
+            {
+                horario: "16:00 - 17:00",
+                jueves: "Cierre de actividades user 1"
+            },
+            {
+                horario: "17:00 - 18:00",
+                viernes: "testear aplicacion user 1"
+            }
+        ]
+    },
+    {
+        name: "user 2",
+        lastname: "lastname 2",
+        phone: 678910,
+        citas: [
+            {
+                horario: "08:00 - 09:00",
+                lunes: "Reunion semanal user 2"
+            },
+            {
+                horario: "09:00 - 10:00",
+                martes: "Inicio de actividades user 2"
+            },
+            {
+                horario: "10:00 - 11:00",
+                miercoles: "Reunion con equipo user 2"
+            },
+            {
+                horario: "11:00 - 12:00",
+                jueves: "Desayuno user 2"
+            },
+            {
+                "horario": "12:00 - 13:00",
+            },
+            {
+                horario: "13:00 - 14:00",
+                viernes: "Actualizar actividades user 2"
+            },
+            {
+                horario: "14:00 - 15:00",
+            },
+            {
+                horario: "15:00 - 16:00",
+                miercoles: "Hora de comida user 2"
+            },
+            {
+                horario: "16:00 - 17:00",
+                jueves: "Cierre de actividades user 2"
+            },
+            {
+                horario: "17:00 - 18:00",
+                viernes: "testear aplicacion user 2"
+            }
+        ]
+    },
+    {
+        name: "user 3",
+        lastname: "lastname 3",
+        phone: 678910,
+        citas: [
+            {
+                horario: "08:00 - 09:00",
+                lunes: "Reunion semanal user 3"
+            },
+            {
+                horario: "09:00 - 10:00",
+                martes: "Inicio de actividades user 3"
+            },
+            {
+                horario: "10:00 - 11:00",
+                miercoles: "Reunion con equipo user 3"
+            },
+            {
+                horario: "11:00 - 12:00",
+                jueves: "Desayuno"
+            },
+            {
+                "horario": "12:00 - 13:00",
+            },
+            {
+                horario: "13:00 - 14:00",
+                viernes: "Actualizar actividades user 3"
+            },
+            {
+                horario: "14:00 - 15:00",
+            },
+            {
+                horario: "15:00 - 16:00",
+                miercoles: "Hora de comida user 3"
+            },
+            {
+                horario: "16:00 - 17:00",
+                jueves: "Cierre de actividades user 3"
+            },
+            {
+                horario: "17:00 - 18:00",
+                viernes: "testear aplicacion user 3"
+            }
+        ]
+    },
+    {
+        name: "user 4",
+        lastname: "lastname 4",
+        phone: 678910,
+        citas: [
+            {
+                horario: "08:00 - 09:00",
+                lunes: "Reunion semanal user 4"
+            },
+            {
+                horario: "09:00 - 10:00",
+                martes: "Inicio de actividades user 4"
+            },
+            {
+                horario: "10:00 - 11:00",
+                miercoles: "Reunion con equipo user 4"
+            },
+            {
+                horario: "11:00 - 12:00",
+                jueves: "Desayuno"
+            },
+            {
+                "horario": "12:00 - 13:00",
+            },
+            {
+                horario: "13:00 - 14:00",
+                viernes: "Actualizar actividades user 4"
+            },
+            {
+                horario: "14:00 - 15:00",
+            },
+            {
+                horario: "15:00 - 16:00",
+                miercoles: "Hora de comida user 4"
+            },
+            {
+                horario: "16:00 - 17:00",
+                jueves: "Cierre de actividades user 4"
+            },
+            {
+                horario: "17:00 - 18:00",
+                viernes: "testear aplicacion user 4"
+            }
+        ]
+    },
+    {
+        name: "user 5",
+        lastname: "lastname 5",
+        phone: 678910,
+        citas: [
+            {
+                horario: "08:00 - 09:00",
+                lunes: "Reunion semanal user 5"
+            },
+            {
+                horario: "09:00 - 10:00",
+                martes: "Inicio de actividades user 5"
+            },
+            {
+                horario: "10:00 - 11:00",
+                miercoles: "Reunion con equipo user 5"
+            },
+            {
+                horario: "11:00 - 12:00",
+                jueves: "Desayuno user 5"
+            },
+            {
+                "horario": "12:00 - 13:00",
+            },
+            {
+                horario: "13:00 - 14:00",
+                viernes: "Actualizar actividades user 5"
+            },
+            {
+                horario: "14:00 - 15:00",
+            },
+            {
+                horario: "15:00 - 16:00",
+                miercoles: "Hora de comida user 5"
+            },
+            {
+                horario: "16:00 - 17:00",
+                jueves: "Cierre de actividades user 5"
+            },
+            {
+                horario: "17:00 - 18:00",
+                viernes: "testear aplicacion user 5"
+            }
+        ]
+    }
+];
